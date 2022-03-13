86cd75708c85b435ebd223d728dfa65cf4122f57
Luiz Gustavo <luiz.ro.aguiar@gmail.com>
2022-03-13 01:14:06 -0300

    Multiple browsers

--- tests/test_search.py
+++ tests/test_search.py
@@ -15,15 +15,15 @@ def test_basic_duckduckgo_search(browser):
    
     # When the user searches for "panda"
     search_page.search(PHRASE)
-   
-    # Then the search result title contains "panda"
-    assert PHRASE in result_page.title()
-   
+
     # And the search result query is "panda"
     assert PHRASE == result_page.search_input_value()
-   
+
     # And the search result links pertain to "panda"
     # for title in result_page.result_link_titles():
     #     assert PHRASE.lower() in title.lower()
-   
+
+    # Then the search result title contains "panda"
+    assert PHRASE in result_page.title()
+
     # raise Exception("Incomplete Test")
