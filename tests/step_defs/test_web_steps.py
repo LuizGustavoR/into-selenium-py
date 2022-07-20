0d9a4bf6b6025693c9b77e9e0ec34fd4ad2c4711
Luiz Gustavo <luiz.ro.aguiar@gmail.com>
2022-07-20 16:58:39 -0300

    Remove unused import

--- tests/step_defs/test_web_steps.py
+++ tests/step_defs/test_web_steps.py
@@ -1,4 +1,4 @@
-from pytest_bdd import scenarios, given, when, then, parsers
+from pytest_bdd import scenarios, when, then, parsers
 
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
